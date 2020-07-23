1022a94629d89be54f50eaff2cc46a9e0064ef31
LaerryLaessig <en-schul@web.de>
2020-07-23 16:24:09 +0200

    use more glyphicon

--- flask_quiz/forms.py
+++ flask_quiz/forms.py
@@ -4,15 +4,12 @@ from wtforms import StringField, SubmitField
 
 class AnswerForm(FlaskForm):
     answer = StringField('answer')
-    submit = SubmitField('send')
 
 
 class NamingForm(FlaskForm):
     name = StringField('name')
-    submit = SubmitField('send')
 
 
 class AddQuestionForm(FlaskForm):
     question = StringField('question')
     answer = StringField('answer')
-    save = SubmitField('save')
