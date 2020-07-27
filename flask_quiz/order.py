247474a5f4635a5afad86294d53f4f95c07edcfb
LaerryLaessig <en-schul@web.de>
2020-07-27 18:57:14 +0200

    rename attribute

--- flask_quiz/order.py
+++ flask_quiz/order.py
@@ -1,21 +1,20 @@
-from flask_quiz.database import get_question_by_id
 from flask_quiz.models import Question
 
 
-def order_question_by_order_number(questions: [Question]):
+def sort_question_by_order_number(questions: [Question]):
     return sorted(questions, key=lambda question: question.order_number)
 
 
-def toggle_postion_in_order(questions: [Question], question: Question, is_new_pos_higher: bool):
+def toggle_postion_in_order(questions: [Question], question_to_toggle: Question, is_new_pos_higher: bool):
     ordered_questions = sorted(questions, key=lambda question: question.order_number)
 
-    index_question = ordered_questions.index(question)
+    index_question = ordered_questions.index(question_to_toggle)
 
-    if is_new_pos_higher is True:
-        ordered_questions.remove(question)
-        ordered_questions.insert(index_question + 1, question)
-    elif is_new_pos_higher is False:
-        ordered_questions.remove(question)
-        ordered_questions.insert(index_question - 1, question)
+    if is_new_pos_higher is True and index_question < len(ordered_questions):
+        ordered_questions.remove(question_to_toggle)
+        ordered_questions.insert(index_question + 1, question_to_toggle)
+    elif is_new_pos_higher is False and index_question > 0:
+        ordered_questions.remove(question_to_toggle)
+        ordered_questions.insert(index_question - 1, question_to_toggle)
 
     return ordered_questions
