b237fae627b9bc6c51336773e0aa37a8718d85f7
LaerryLaessig <en-schul@web.de>
2020-11-12 20:51:17 +0100

    revert wordcloud

--- flask_quiz/charts.py
+++ flask_quiz/charts.py
@@ -1,5 +1,4 @@
 import os
-from wordcloud import WordCloud
 
 from flask_quiz import app
 from flask_quiz.models import Answer, Question
@@ -11,8 +10,6 @@ def generate_wordcloud_img(answers: [Answer]):
     is_wordcloud_generated = False
     if len(words) > 0:
         try:
-            wordcloud = WordCloud(width=1024, height=1024, margin=0).generate(' '.join(words))
-            wordcloud.to_file('{}/wordcloud.png'.format(os.path.join(app.config['IMAGE_FOLDER'])))
             is_wordcloud_generated = True
         except ValueError:
             is_wordcloud_generated = False
