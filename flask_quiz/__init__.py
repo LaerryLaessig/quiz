2cb1c91726cb711da4debf1793d0528dcffa1a39
laerrylaessig <laerrylaessig@gmail.com>
2023-05-06 12:07:05 +0200

    update dependencies

--- flask_quiz/__init__.py
+++ flask_quiz/__init__.py
@@ -9,21 +9,21 @@ PWD_ADMIN = os.getenv('PWD_ADMIN')
 IMAGE_FOLDER = os.path.join('flask_quiz', 'static', 'images')
 
 app = Flask(__name__)
+with app.app_context():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_quiz.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_quiz.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['IMAGE_FOLDER'] = IMAGE_FOLDER
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+    Bootstrap(app)
 
-Bootstrap(app)
+    db = SQLAlchemy(app)
 
-db = SQLAlchemy(app)
+    from flask_quiz import database
+    from flask_quiz import models
+    from flask_quiz import routes
+    from flask_quiz.errors.handlers import errors
+    app.register_blueprint(errors)
 
-from flask_quiz import database
-from flask_quiz import models
-from flask_quiz import routes
-from flask_quiz.errors.handlers import errors
-app.register_blueprint(errors)
-
-database.create_database()
+    database.create_database()
